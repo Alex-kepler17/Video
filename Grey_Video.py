f70177465f65ce97ebd0123d38671cd56f1e7daa
Alex-kepler17 <113709443+Alex-kepler17@users.noreply.github.com>
2022-09-20 23:51:29 +0700

    Video

---
+++ Grey_Video.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('video feed', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('gray feed', gray)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # При нажатии 'q' программа завершается
+        break
+
+cap.release()
+cv2.destroyAllWindows()
